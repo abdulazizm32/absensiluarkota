3bdfdb00a393b7a1c69fce7e8978c1657fb8e7c0
abdul <abdul@cendana.id>
2026-01-23 01:23:27 +0700

    [FIX] fitur terbaru sampai sabsen tetapi belum sampai foto

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,8 +18,11 @@ dependencyResolutionManagement {
         mavenCentral()
         maven {
             url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            credentials {
+                username = "mapbox"
+                password = "pk.eyJ1IjoiYWJkdWxheml6bXVydGFkaG8zMiIsImEiOiJjbWszZzdtdGQwcmZpM2RwdnAxeXdmNnRtIn0.HCe_tAXTv9i9c7WugsH0kw"
+            }
         }
-
     }
 }
 
